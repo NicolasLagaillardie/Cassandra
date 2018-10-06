74980d1c20625a5682c8ef2d99701d33b9bdb231
NicolasLagaillardie <lagaillardie.nicolas@live.fr>
2018-10-07 00:22:13 +0200

    Report

--- first.py
+++ first.py
@@ -6,93 +6,67 @@ Created on Wed Oct  3 10:03:42 2018
 @author: lag
 """
 
+"""Utilisation de Cassandra à travers Python"""
 from cassandra.cluster import Cluster
+
+"""Pour la génération aléatoire de chaîne de caractères"""
+from random import choice,randint
+import string
 
 cluster = Cluster()
 session = cluster.connect()
 
-"""Create the keyspace before"""
-session.set_keyspace('users')
+#Sélectionne le keyspace demo
+session.set_keyspace('demo')
     
+#Ajout massif d'entrées
+for i in range(200):
+
+    idEleve = str(i)
+    nom = "".join(choice(string.ascii_letters) for x in range(randint(5, 12)))
+    prenom = "".join(choice(string.ascii_letters) for x in range(randint(5, 12)))
+    nom = nom.upper()
+    prenom = prenom.capitalize()
+    mail = prenom.lower() + "." + nom.lower() + "@etu.emse.fr"
+    telephone = "".join(choice(string.digits) for x in range(10))
+    
+    session.execute(
+        """
+        INSERT INTO eleves (idEleves, nom, prenom, mail, telephone)
+        VALUES (%s, %s, %s, %s, %s)
+        """,
+        (idEleve, nom, prenom, mail, telephone)
+    )
+    
+#Ajout d'une valeur connue
 session.execute(
     """
-    INSERT INTO users (firstname, lastname, age, email, city)
+    INSERT INTO eleves (idEleves, nom, prenom, mail, telephone)
     VALUES (%s, %s, %s, %s, %s)
     """,
-    ("Nicolas", "LAGAILLARDIE", 24, "contact@minbot.fr", "Sainte")
+    ("500", "Lag", "Nicolas", "Nicolas.LAGAILLARDIE@etu.emse.fr", "0612345678")
 )
 
-session.execute(
-    """
-    INSERT INTO users (firstname, lastname, age, email, city)
-    VALUES (%s, %s, %s, %s, %s)
-    """,
-    ("Chloe", "GOURRAT", 22, "chloeG@minbot.fr", "Sainte")
-)
-
-session.execute(
-    """
-    INSERT INTO users (firstname, lastname, age, email, city, notes, notestxt)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
-    """,
-    ("Chloe", "POURIDGE", 25, "chloeP@minbot.fr", "Sainte", [15,16], ["15"])
-)
-
-session.execute(
-    """
-    INSERT INTO users (firstname, lastname, age, email, city)
-    VALUES (%s, %s, %s, %s, %s)
-    """,
-    ("Chloe", "SAFRAN", 22, "chloeS@minbot.fr", "Sainte")
-)
-
-session.execute(
-    """
-    INSERT INTO users (firstname, lastname, age, email, city)
-    VALUES (%s, %s, %s, %s, %s)
-    """,
-    ("Aubin", "RABOUAN", 23, "aubin@minbot.fr", "Sainte")
-)
-
-session.execute(
-    """
-    INSERT INTO users (firstname, lastname, age, email, city)
-    VALUES (%s, %s, %s, %s, %s)
-    """,
-    ("Paul", "BREUGNOT", 23, "PBR@minbot.fr", "Sainte")
-)
-
-session.execute(
-    """
-    INSERT INTO users (firstname, lastname, age, email, city)
-    VALUES (%s, %s, %s, %s, %s)
-    """,
-    ("Paul", "BONNET", 23, "PBE@minbot.fr", "Sainte")
-)
-
-"""Create the table before"""
-rows = session.execute('SELECT firstname, lastname, age FROM users')
+#Affichage de tous les noms, prénoms et mails
+rows = session.execute('SELECT nom, prenom, mail FROM eleves')
 for user_row in rows:
-    print(user_row.firstname, user_row.lastname, user_row.age)
+    print(user_row.nom, user_row.prenom, user_row.mail)
     
-rows = session.execute('SELECT firstname, lastname, age FROM users')
+#Affichage de idEleves, cours et mail
+rows = session.execute('SELECT * FROM eleves')
 for row in rows:
     print(row[0], row[1], row[2])
 
-rows = session.execute('SELECT firstname, lastname, age FROM users')
-for user_row in rows:
-    print(user_row.firstname, user_row.lastname, user_row.age)
-
-# build a list of futures
+# Affichage des entrées dont le prénom est Nicolas à l'aide d'un requête asynchrone
 futures = []
-query = "SELECT * FROM users WHERE firstname=%s  ALLOW FILTERING"
-FN_to_fetch = ["Chloe"]
-for firstname in FN_to_fetch:
-    futures.append(session.execute_async(query, [firstname]))
+query = "SELECT * FROM eleves WHERE prenom=%s  ALLOW FILTERING"
+FN_to_fetch = ["Nicolas"]
+for prenom in FN_to_fetch:
+    futures.append(session.execute_async(query, [prenom]))
     
 print()
 
-# wait for them to complete and use the results
+# Attente puis affichage des résultats asynchrones
 for future in futures:
     rows = future.result()
     for row in rows :
@@ -100,4 +74,3 @@ for future in futures:
             print(elt, end="")
             print(" ", end="")
         print()
-        
